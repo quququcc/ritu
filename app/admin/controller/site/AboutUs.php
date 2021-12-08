103839e53f93943db731edc33c4958c7a65634ce
weiming <weiming.wei@feisu.com>
2021-12-08 18:41:30 +0800

    ing

---
+++ app/admin/controller/site/AboutUs.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace app\admin\controller\site;
+
+use app\common\controller\AdminController;
+use EasyAdmin\annotation\ControllerAnnotation;
+use EasyAdmin\annotation\NodeAnotation;
+use think\App;
+
+/**
+ * @ControllerAnnotation(title="site_about_us")
+ */
+class AboutUs extends AdminController
+{
+
+    use \app\admin\traits\Curd;
+
+    public function __construct(App $app)
+    {
+        parent::__construct($app);
+
+        $this->model = new \app\admin\model\SiteAboutUs();
+        
+    }
+
+    /**
+     * @NodeAnotation(title="列表")
+     */
+    public function index()
+    {
+        $data = $this->model->where('id',1)->find()->toArray();
+        $this->assign('data',$data);
+        return $this->fetch();
+    }
+
+    /**
+     * @NodeAnotation(title="保存")
+     */
+    public function save()
+    {
+        $this->checkPostRequest();
+        $post = $this->request->post();
+        try {
+            $update = [
+                'text'=>$post['text'],
+                'create_time'=>$post['create_time'] ?? 0,
+                'service_num'=>$post['service_num'] ?? 0,
+                'staff_num'=>$post['staff_num'] ?? 0,
+                'filiale_num'=>$post['filiale_num'] ?? 0,
+            ];
+            $this->model->where('id',1)->update($update);
+        } catch (\Exception $e) {
+            $this->error('保存失败');
+        }
+        $this->success('保存成功');
+    }
+}
---
+++ app/admin/model/SiteAboutUs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\admin\model;
+
+use app\common\model\TimeModel;
+
+class SiteAboutUs extends TimeModel
+{
+
+    protected $name = "site_about_us";
+
+    protected $deleteTime = false;
+
+    
+    
+
+}
