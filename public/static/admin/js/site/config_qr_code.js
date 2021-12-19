d72e0d6f4d272f7b60091c0e62e9dc9a0ef21a82
Weiming <Weiming.Wei@feisu.com>
2021-12-20 01:31:03 +0800

    开发ing

---
+++ public/static/admin/js/site/config_qr_code.js
@@ -0,0 +1,34 @@
+define(["jquery", "easy-admin"], function ($, ea) {
+
+    var init = {
+        table_elem: '#currentTable',
+        table_render_id: 'currentTableRenderId',
+        index_url: 'site.config_qr_code/index',
+        add_url: 'site.config_qr_code/add',
+        edit_url: 'site.config_qr_code/edit',
+        delete_url: 'site.config_qr_code/delete',
+        export_url: 'site.config_qr_code/export',
+        modify_url: 'site.config_qr_code/modify',
+    };
+
+    var Controller = {
+
+        index: function () {
+            ea.table.render({
+                init: init,
+                cols: [[
+                    {type: 'checkbox'},                    {field: 'id', title: '配置标识'},                    {field: 'icon', title: '第三方图标', templet: ea.table.image},                    {field: 'qr_code', title: '展示二维码', templet: ea.table.image},                    {field: 'sort', title: '排序', edit: 'text'},                    {width: 250, title: '操作', templet: ea.table.tool},
+                ]],
+            });
+
+            ea.listen();
+        },
+        add: function () {
+            ea.listen();
+        },
+        edit: function () {
+            ea.listen();
+        },
+    };
+    return Controller;
+});
